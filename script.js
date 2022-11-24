52d1d7a2a8367e770c5d37ee9a858ac90db922e1
sundaymark <sundayukpai1122@gmail.com>
2022-11-24 20:50:44 +0100

    add the file above

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const num1 = document.querySelector('.num1');
 const num2 = document.querySelector('.num2');
 const add = document.querySelector('.add');
+add.addEventListener('click', function(){
+    if(num1.value === '' && num2.value === ''){
+        alert('please add an operator');
+    }
+})
